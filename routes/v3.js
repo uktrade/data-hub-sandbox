1bc5ae0683e0246d4fd36ffd5d89db9cf6bdc271
Pippo Raimondi <filipporai@gmail.com>
2019-02-15 19:12:18 +0000

    add archived company to test interaction behaviour

--- routes/v3.js
+++ routes/v3.js
@@ -1,5 +1,6 @@
 var featureFlag = require('../fixtures/v3/feature-flag.json')
 var company = require('../fixtures/v3/company.json')
+var archivedCompany = require('../fixtures/v3/archived-company.json')
 var companies = require('../fixtures/v3/search/company.json')
 var interaction = require('../fixtures/v3/interaction.json')
 var contacts = require('../fixtures/v3/search/contact.json')
@@ -8,6 +9,7 @@ var singleContact = require('../fixtures/v3/single-contact.json')
 var singleCompany = require('../fixtures/v3/single-company.json')
 var singleInteraction = require('../fixtures/v3/single-interaction.json')
 var contactInteraction = require('../fixtures/v3/contact-interaction.json')
+var archivedCompanyInteraction = require('../fixtures/v3/archived-company-interaction.json')
 var createInteraction = require('../fixtures/v3/create-interaction.json')
 var events = require('../fixtures/v3/search/events.json')
 
@@ -27,6 +29,12 @@ exports.interaction = function (req, res) {
   if(req.query.contact_id) {
     return res.json(contactInteraction)
   }
+
+  if(req.query.company_id === '346f78a5-1d23-4213-b4c2-bf48246a13c3')
+  {
+    return res.json(archivedCompanyInteraction)
+  }
+
   res.json(interaction)
 }
 
@@ -51,6 +59,9 @@ exports.singleContact = function (req, res) {
 }
 
 exports.singleCompany = function (req, res) {
+  if(req.params.companyId === '4cd4128b-1bad-4f1e-9146-5d4678c6a018') {
+    return res.json(archivedCompany)
+  }
   res.json(singleCompany)
 }
 
