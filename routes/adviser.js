347f3220c29f762f255cafd28267d92d330781d5
Pippo Raimondi <filipporai@gmail.com>
2019-02-18 12:23:32 +0000

    add interaction fixtures and routes

--- routes/adviser.js
+++ routes/adviser.js
@@ -1,5 +1,10 @@
-var adviser = require('../fixtures/adviser.json')
+var advisers = require('../fixtures/adviser.json')
+var singleAdviser = require('../fixtures/single-adviser.json')
 
-exports.adviser = function (req, res) {
-    res.json(adviser)
+exports.advisers = function (req, res) {
+    res.json(advisers)
+}
+
+exports.singleAdviser = function (req, res) {
+    res.json(singleAdviser)
 }
