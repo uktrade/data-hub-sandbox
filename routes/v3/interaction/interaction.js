548f1272dfedbe6d6a3ee63c586eb03d788e6a36
Pippo Raimondi <filipporai@gmail.com>
2019-02-26 12:45:43 +0000

    refactor folder structure and naming convention to ease scalability

---
+++ routes/v3/interaction/interaction.js
@@ -0,0 +1,40 @@
+var interaction = require('../../../fixtures/v3/interaction/interaction.json')
+var interactions = require('../../../fixtures/v3/interaction/interactions.json')
+var interactionByInvestmentProjectId = require('../../../fixtures/v3/interaction/interaction-by-investment-project-id.json')
+var interactionByCompanyId = require('../../../fixtures/v3/interaction/interaction-by-company-id.json')
+var interactionByContactId = require('../../../fixtures/v3/interaction/interaction-by-contact-id.json')
+var interactionCreate = require('../../../fixtures/v3/interaction/interaction-create.json')
+
+state.interactionSubject = state.interactionSubject || []
+
+exports.interactions = function (req, res) {
+  if(req.query.contact_id) {
+    return res.json(interactionByContactId)
+  }
+
+  if(req.query.company_id === '346f78a5-1d23-4213-b4c2-bf48246a13c3')
+  {
+    return res.json(interactionByCompanyId)
+  }
+
+  if(req.query.investment_project_id === '00086bb2-c5ee-4bfb-998f-0b417eafdd3e')
+  {
+    return res.json(interactionByInvestmentProjectId)
+  }
+
+  res.json(interactions)
+}
+
+exports.interactionById = function (req, res) {
+  if (state.interactionSubject) {
+    return res.json(_.merge(interaction, {"subject": state.interactionSubject}))
+  }
+  res.json(interaction)
+}
+
+exports.interactionCreate = function (req, res) {
+  if (req.body.subject) {
+    state.interactionSubject = req.body.subject
+  }
+  res.json(interactionCreate)
+}
