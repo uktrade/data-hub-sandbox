abbe2d02db0ef5c327e1b96af68da07f0428bd2b
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-03-11 13:22:23 +0000

    Add One List Corp company mock data

--- routes/v3/company/company.js
+++ routes/v3/company/company.js
@@ -3,23 +3,19 @@ var companies = require('../../../fixtures/v3/company/companies.json')
 var companyArchived = require('../../../fixtures/v3/company/company-archived.json')
 var companyWithInvestment1 = require('../../../fixtures/v3/company/company-with-investment-1.json')
 var companyWithInvestment2 = require('../../../fixtures/v3/company/company-with-investment-2.json')
+var companyOneListCorp = require('../../../fixtures/v3/company/company-one-list-corp.json')
 
 exports.companies = function (req, res) {
   res.json(companies)
 }
 
 exports.company = function (req, res) {
-  if(req.params.companyId === '4cd4128b-1bad-4f1e-9146-5d4678c6a018') {
-    return res.json(companyArchived)
+  var companies = {
+    '4cd4128b-1bad-4f1e-9146-5d4678c6a018': companyArchived,
+    '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
+    'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
+    '375094ac-f79a-43e5-9c88-059a7caa17f0': companyOneListCorp,
   }
 
-  if(req.params.companyId === '0f5216e0-849f-11e6-ae22-56b6b6499611') {
-    return res.json(companyWithInvestment1)
-  }
-
-  if(req.params.companyId === 'a73efeba-8499-11e6-ae22-56b6b6499611') {
-    return res.json(companyWithInvestment2)
-  }
-
-  res.json(company)
+  res.json(companies[req.params.companyId] || company)
 }
