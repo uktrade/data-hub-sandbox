3a601c7e19a3add024ea871fedbe85879eaeeeb3
Ian Leggett <contactleggett@gmail.com>
2019-03-12 12:24:42 +0000

    Add adviser autocomplete fixture and fixing archived id

--- routes/adviser.js
+++ routes/adviser.js
@@ -1,7 +1,11 @@
 var advisers = require('../fixtures/adviser.json')
+var autoCompleteAdvisor = require('../fixtures/autocomplete-adviser.json')
 var singleAdviser = require('../fixtures/single-adviser.json')
 
 exports.advisers = function (req, res) {
+    if (req.query.autocomplete) {
+        return res.json(autoCompleteAdvisor)
+    }
     res.json(advisers)
 }
 
