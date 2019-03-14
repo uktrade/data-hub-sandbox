9dddd0c155bb384244e04de83343fa79788cb28b
Paul Gain <paul.gain@javexconsulting.com>
2019-03-14 22:45:08 +0000

    Adding a v4 Large capital profile endpoint

---
+++ routes/v4/company/company.js
@@ -0,0 +1,6 @@
+var largeCapitalProfile = require('../../../fixtures/v4/company/large-capital-profile.json')
+
+exports.largeInvestorProfile = function (req, res) {
+  res.json(largeCapitalProfile)
+}
+
