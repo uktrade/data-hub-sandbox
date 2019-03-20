9ecc4761678b95761c90181558019eca530d5684
Pippo Raimondi <filipporai@gmail.com>
2019-03-20 16:33:49 +0000

    Add document fixtures for contact

--- routes/v3/contact/contact.js
+++ routes/v3/contact/contact.js
@@ -1,10 +1,15 @@
 var contact = require('../../../fixtures/v3/contact/contact.json')
 var contactById = require('../../../fixtures/v3/contact/contact-by-id.json')
+var contactByIdWithNoDocument = require('../../../fixtures/v3/contact/contact-by-id-with-no-document.json')
 
 exports.contact = function (req, res) {
   res.json(contact)
 }
 
 exports.contactById = function (req, res) {
-  res.json(contactById)
+  var contacts = {
+    '5555d636-1d24-416a-aaf0-3fb220d59aaa': contactByIdWithNoDocument,
+  }
+
+  res.json(contacts[req.params.contactId] || contactById)
 }
