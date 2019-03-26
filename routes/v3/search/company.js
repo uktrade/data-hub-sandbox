20748e1d8f3ba822ba37b276f0907f17e0e2d022
Pippo Raimondi <filipporai@gmail.com>
2019-03-26 10:42:12 +0000

    Add company fixture

--- routes/v3/search/company.js
+++ routes/v3/search/company.js
@@ -1,9 +1,14 @@
 var companies = require('../../../fixtures/v3/search/company.json')
 var companyWithAttributes = require('../../../fixtures/v3/search/company-with-attributes.json')
+var companyFilterByName = require('../../../fixtures/v3/search/filter/company-filter-by-name.json')
 
 exports.companies = function (req, res) {
   companiesList = {
     'collectionTest': companyWithAttributes,
+  }
+
+  if (req.body.name || req.body.archived || req.body.country || req.body.uk_region) {
+    return res.json(companyFilterByName)
   }
 
   res.json(companiesList[req.body.sortby] || companies)
