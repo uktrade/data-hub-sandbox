46c5792357d755c05e6492cf882c183dcb69a37c
Pippo Raimondi <filipporai@gmail.com>
2019-04-01 11:00:03 +0100

    Add contacts sort fixture

--- routes/v3/search/contact.js
+++ routes/v3/search/contact.js
@@ -1,17 +1,25 @@
 var contacts = require('../../../fixtures/v3/search/contact.json')
 var contactFilter = require('../../../fixtures/v3/search/filter/contact-filter.json')
 var contactWithAttributes = require('../../../fixtures/v3/search/contact-with-attributes.json')
+var contactSortBy = require('../../../fixtures/v3/search/sort/contact-sort-by.json')
 
 exports.contacts = function (req, res) {
   contactsList = {
     'collectionTest': contactWithAttributes,
+    'created_on:desc': contactSortBy,
+    'created_on:asc': contactSortBy,
+    'modified_on:desc': contactSortBy,
+    'modified_on:asc': contactSortBy,
+    'address_country.name:asc': contactSortBy,
+    'company.name:asc': contactSortBy,
+    'last_name:asc': contactSortBy,
   }
 
   if (req.body.name === 'FilterByContacts' ||
     req.body.archived === 'false' ||
     req.body.archived === 'true' ||
-    req.body.country === '87756b9a-5d95-e211-a939-e4115bead28a' ||
-    req.body.uk_region === '934cd12a-6095-e211-a939-e4115bead28a') {
+    req.body.address_country === '87756b9a-5d95-e211-a939-e4115bead28a' ||
+    req.body.company_uk_region === '934cd12a-6095-e211-a939-e4115bead28a') {
     return res.json(contactFilter)
   }
 
