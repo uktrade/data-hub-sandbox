3908dba45da2aa9bf6696ae11319ff3d64c998f3
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-04-04 18:13:19 +0100

    Add minimal company mock

--- routes/v3/company/company.js
+++ routes/v3/company/company.js
@@ -1,10 +1,11 @@
 var company = require('../../../fixtures/v3/company/company.json')
-var companies = require('../../../fixtures/v3/company/companies.json')
+var companiesNoResults = require('../../../fixtures/v3/company/companies-no-results.json')
 var companyArchived = require('../../../fixtures/v3/company/company-archived.json')
 var companyArchivedSubsidiaries = require('../../../fixtures/v3/company/company-archived-subsidiaries.json')
 var companyWithInvestment1 = require('../../../fixtures/v3/company/company-with-investment-1.json')
 var companyWithInvestment2 = require('../../../fixtures/v3/company/company-with-investment-2.json')
 var companyDnBCorp = require('../../../fixtures/v3/company/company-dnb-corp.json')
+var companyMinimallyMinimal = require('../../../fixtures/v3/company/company-minimally-minimal.json')
 var companyOneListCorp = require('../../../fixtures/v3/company/company-one-list-corp.json')
 
 exports.companies = function (req, res) {
@@ -12,7 +13,7 @@ exports.companies = function (req, res) {
     '346f78a5-1d23-4213-b4c2-bf48246a13c3': companyArchivedSubsidiaries,
   }
 
-  res.json(subsidiaries[req.query.global_headquarters_id] || companies)
+  res.json(subsidiaries[req.query.global_headquarters_id] || companiesNoResults)
 }
 
 exports.company = function (req, res) {
@@ -21,6 +22,7 @@ exports.company = function (req, res) {
     '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
     'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
     'cc7e2f19-7251-4a41-a27a-f98437720531': companyDnBCorp,
+    '149475d4-13a0-e511-88b6-e4115bead28a': companyMinimallyMinimal,
     '375094ac-f79a-43e5-9c88-059a7caa17f0': companyOneListCorp,
   }
 
--- routes/v4/company/company.js
+++ routes/v4/company/company.js
@@ -3,6 +3,7 @@ var companyArchived = require('../../../fixtures/v4/company/company-archived.jso
 var companyWithInvestment1 = require('../../../fixtures/v4/company/company-with-investment-1.json')
 var companyWithInvestment2 = require('../../../fixtures/v4/company/company-with-investment-2.json')
 var companyDnBCorp = require('../../../fixtures/v4/company/company-dnb-corp.json')
+var companyMinimallyMinimal = require('../../../fixtures/v4/company/company-minimally-minimal.json')
 var companyOneListCorp = require('../../../fixtures/v4/company/company-one-list-corp.json')
 
 var nolargeCapitalProfile = require('../../../fixtures/v4/company/large-capital-profile-empty.json')
@@ -22,6 +23,7 @@ exports.company = function (req, res) {
     '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
     'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
     'cc7e2f19-7251-4a41-a27a-f98437720531': companyDnBCorp,
+    '149475d4-13a0-e511-88b6-e4115bead28a': companyMinimallyMinimal,
     '375094ac-f79a-43e5-9c88-059a7caa17f0': companyOneListCorp,
   }
 
