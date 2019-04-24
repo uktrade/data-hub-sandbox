d5718881bc6d15162a829995eeb60adf607d1b2d
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-04-24 13:14:23 +0100

    Add company scenarios for testing edit view

--- routes/v4/company/company.js
+++ routes/v4/company/company.js
@@ -1,10 +1,12 @@
 var company = require('../../../fixtures/v4/company/company.json')
 var companyArchived = require('../../../fixtures/v4/company/company-archived.json')
-var companyWithInvestment1 = require('../../../fixtures/v4/company/company-with-investment-1.json')
-var companyWithInvestment2 = require('../../../fixtures/v4/company/company-with-investment-2.json')
 var companyDnBCorp = require('../../../fixtures/v4/company/company-dnb-corp.json')
+var companyLambdaPlc = require('../../../fixtures/v4/company/company-lambda-plc.json')
+var companyMarsExportsLtd = require('../../../fixtures/v4/company/company-mars-exports-ltd.json')
 var companyMinimallyMinimal = require('../../../fixtures/v4/company/company-minimally-minimal.json')
 var companyOneListCorp = require('../../../fixtures/v4/company/company-one-list-corp.json')
+var companyWithInvestment1 = require('../../../fixtures/v4/company/company-with-investment-1.json')
+var companyWithInvestment2 = require('../../../fixtures/v4/company/company-with-investment-2.json')
 
 var nolargeCapitalProfile = require('../../../fixtures/v4/company/large-capital-profile-empty.json')
 var largeCapitalProfile = require('../../../fixtures/v4/company/large-capital-profile.json')
@@ -24,11 +26,13 @@ exports.largeInvestorProfilePatched = function (req, res) {
 exports.company = function (req, res) {
   var companies = {
     '346f78a5-1d23-4213-b4c2-bf48246a13c3': companyArchived,
-    '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
-    'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
     'cc7e2f19-7251-4a41-a27a-f98437720531': companyDnBCorp,
+    '0fb3379c-341c-4da4-b825-bf8d47b26baa': companyLambdaPlc,
+    'b2c34b41-1d5a-4b4b-9249-7c53ff2868dd': companyMarsExportsLtd,
     '149475d4-13a0-e511-88b6-e4115bead28a': companyMinimallyMinimal,
     '375094ac-f79a-43e5-9c88-059a7caa17f0': companyOneListCorp,
+    '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
+    'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
   }
 
   res.json(companies[req.params.companyId] || company)
