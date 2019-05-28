3941f19c656a53fdad3bc3b5dbef280d92f81c92
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-05-28 11:28:58 +0100

    Update interaction fixtures

--- routes/v3/interaction/interaction.js
+++ routes/v3/interaction/interaction.js
@@ -6,6 +6,8 @@ var interactionByInvestmentProjectId = require('../../../fixtures/v3/interaction
 var interactionByCompanyId = require('../../../fixtures/v3/interaction/interaction-by-company-id.json')
 var interactionByContactId = require('../../../fixtures/v3/interaction/interaction-by-contact-id.json')
 var interactionCreate = require('../../../fixtures/v3/interaction/interaction-create.json')
+var interactionDraftFutureMeeting = require('../../../fixtures/v3/interaction/interaction-draft-future-meeting.json')
+var interactionDraftPastMeeting = require('../../../fixtures/v3/interaction/interaction-draft-past-meeting.json')
 
 state.interactionSubject = state.interactionSubject || []
 
@@ -14,13 +16,11 @@ exports.interactions = function (req, res) {
     return res.json(interactionByContactId)
   }
 
-  if(req.query.company_id === '346f78a5-1d23-4213-b4c2-bf48246a13c3')
-  {
+  if(req.query.company_id === '346f78a5-1d23-4213-b4c2-bf48246a13c3') {
     return res.json(interactionByCompanyId)
   }
 
-  if(req.query.investment_project_id === '5d341b34-1fc8-4638-b4b1-a0922ebf401e')
-  {
+  if(req.query.investment_project_id === '5d341b34-1fc8-4638-b4b1-a0922ebf401e') {
     return res.json(interactionByInvestmentProjectId)
   }
 
@@ -31,6 +31,8 @@ exports.interactionById = function (req, res) {
   var interactions = {
     'ec4a46ef-6e50-4a5c-bba0-e311f0471312': interactionWithDocumentLink,
     '0dcb3748-c097-4f20-b84f-0114bbb1a8e0': interactionWithoutDocumentLink,
+    '999c12ee-91db-4964-908e-0f18ce823096': interactionDraftFutureMeeting,
+    '888c12ee-91db-4964-908e-0f18ce823096': interactionDraftPastMeeting,
   }
 
   if (state.interactionSubject) {
