0aad2943a149c8fb527cbf1a9fffcac8c2ccdc4a
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-13 16:52:10 +0100

    Add endpoint for DnB company create investigation

--- routes/v4/dnb/index.js
+++ routes/v4/dnb/index.js
@@ -1,5 +1,6 @@
 var companyCreate = require('../../../fixtures/v4/dnb/company-create.json')
 var companySearch = require('../../../fixtures/v4/dnb/company-search.json')
+var companyCreateInvestigation = require('../../../fixtures/v4/dnb/company-create-investigation.json')
 
 exports.companyCreate = function (req, res) {
   res.json(companyCreate)
@@ -7,4 +8,8 @@ exports.companyCreate = function (req, res) {
 
 exports.companySearch = function (req, res) {
   res.json(companySearch)
+}
+
+exports.companyCreateInvestigation = function (req, res) {
+  res.json(companyCreateInvestigation)
 }
