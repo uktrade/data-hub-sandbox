cc0d3f7fae16bca7baa553b826a9f78a3036b360
Lee Wilson <lee.wilson@coolestbananas.co.uk>
2019-09-16 16:49:11 +0100

    Add company fixtures

--- routes/v4/company/company.js
+++ routes/v4/company/company.js
@@ -3,10 +3,12 @@ var companyArchivedSubsidiaries = require('../../../fixtures/v4/company/company-
 var company = require('../../../fixtures/v4/company/company.json')
 var companyArchived = require('../../../fixtures/v4/company/company-archived.json')
 var companyDnBCorp = require('../../../fixtures/v4/company/company-dnb-corp.json')
+var companyInvestigationLtd = require('../../../fixtures/v4/company/company-investigation-ltd.json')
 var companyLambdaPlc = require('../../../fixtures/v4/company/company-lambda-plc.json')
 var companyMarsExportsLtd = require('../../../fixtures/v4/company/company-mars-exports-ltd.json')
 var companyMinimallyMinimal = require('../../../fixtures/v4/company/company-minimally-minimal.json')
 var companyOneListCorp = require('../../../fixtures/v4/company/company-one-list-corp.json')
+var companySomeOtherCompany = require('../../../fixtures/v4/company/company-some-other-company.json')
 var companyWithInvestment1 = require('../../../fixtures/v4/company/company-with-investment-1.json')
 var companyWithInvestment2 = require('../../../fixtures/v4/company/company-with-investment-2.json')
 var companyWithContacts = require('../../../fixtures/v4/company/company-with-contacts.json')
@@ -54,10 +56,12 @@ exports.company = function (req, res) {
   var companies = {
     '346f78a5-1d23-4213-b4c2-bf48246a13c3': companyArchived,
     'cc7e2f19-7251-4a41-a27a-f98437720531': companyDnBCorp,
+    'ca8fae21-2895-47cf-90ba-9273c94dab81': companyInvestigationLtd,
     '0fb3379c-341c-4da4-b825-bf8d47b26baa': companyLambdaPlc,
     'b2c34b41-1d5a-4b4b-9249-7c53ff2868dd': companyMarsExportsLtd,
     '149475d4-13a0-e511-88b6-e4115bead28a': companyMinimallyMinimal,
     '375094ac-f79a-43e5-9c88-059a7caa17f0': companyOneListCorp,
+    'ca8fae21-2895-47cf-90ba-9273c94dab92': companySomeOtherCompany,
     '0f5216e0-849f-11e6-ae22-56b6b6499611': companyWithInvestment1,
     'a73efeba-8499-11e6-ae22-56b6b6499611': companyWithInvestment2,
     '0f5216e0-849f-11e6-ae22-56b6b6499622': companyWithContacts,
