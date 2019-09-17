1e451ea7ff15ac19719f3375f52536186eacd451
Reupen Shah <reupen@users.noreply.github.com>
2019-09-17 11:24:37 +0100

    Add new company list routes and fixture

--- routes/v4/company-list/companyList.js
+++ routes/v4/company-list/companyList.js
@@ -1,5 +1,24 @@
 var companyList = require("../../../fixtures/v4/company-list/company-list.json");
+var multipleItemCompanyList = require("../../../fixtures/v4/company-list/single-list-with-multiple-items.json");
 
 exports.companyList = function(req, res) {
   res.json(companyList);
+};
+
+exports.getCompanyList = function(req, res) {
+  if (req.params.listId === multipleItemCompanyList.id) {
+    res.json(multipleItemCompanyList);
+    return;
+  }
+
+  res.send(404);
+};
+
+exports.deleteCompanyList = function(req, res) {
+  if (req.params.listId === multipleItemCompanyList.id) {
+    res.send(204);
+    return;
+  }
+
+  res.send(404);
 };
