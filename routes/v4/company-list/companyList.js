099a4681aaece8361c982d8bf3eeb0dbbd44f4ab
Reupen Shah <reupen@users.noreply.github.com>
2019-09-18 14:45:59 +0100

    Add a company list that returns an error on delete

--- routes/v4/company-list/companyList.js
+++ routes/v4/company-list/companyList.js
@@ -1,5 +1,6 @@
 var companyList = require("../../../fixtures/v4/company-list/company-list.json");
 var multipleItemCompanyList = require("../../../fixtures/v4/company-list/single-list-with-multiple-items.json");
+var errorOnDeleteCompanyList = require("../../../fixtures/v4/company-list/single-list-with-error-on-delete.json");
 
 exports.companyList = function(req, res) {
   res.json(companyList);
@@ -8,6 +9,11 @@ exports.companyList = function(req, res) {
 exports.getCompanyList = function(req, res) {
   if (req.params.listId === multipleItemCompanyList.id) {
     res.json(multipleItemCompanyList);
+    return;
+  }
+
+  if (req.params.listId === errorOnDeleteCompanyList.id) {
+    res.json(errorOnDeleteCompanyList);
     return;
   }
 
